6b1af83aecfe28078d8552bb0dd7cdf252a0fe4e
mogwai <feelirabs@gmail.com>
2019-10-29 05:23:49 +0100

    Initialized Express, imported data

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res)=> {
+    res.send("Hello, Nurse!");
+})
+
+server.listen(7000, ()=> console.log('Server eavesdropping on port 7000'))
